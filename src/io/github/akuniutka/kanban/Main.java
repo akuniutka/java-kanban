9be18e607274f0f7c59f6bd5d3f18bd7379a5ea0
Andrei Kuniutka <akuniutka@gmail.com>
2024-03-21 20:11:32 +0300

    gave TaskManager methods more clear names; refactored logged messages; refactored epic status updating method

--- src/io/github/akuniutka/kanban/Main.java
+++ src/io/github/akuniutka/kanban/Main.java
@@ -28,35 +28,29 @@ public class Main {
         Task getHomeworkTask = new Task();
         getHomeworkTask.setTitle("Get homework");
 
-        studyTheoryTask = taskManager.createNewTask(studyTheoryTask);
-        getHomeworkTask = taskManager.createNewTask(getHomeworkTask);
+        studyTheoryTask = taskManager.addTask(studyTheoryTask);
+        getHomeworkTask = taskManager.addTask(getHomeworkTask);
         printAllTasks(taskManager);
         System.out.println();
 
-        studyTheoryTask = taskManager.getTaskById(studyTheoryTask.getId());
-        System.out.println(studyTheoryTask);
-        getHomeworkTask = taskManager.getTaskById(getHomeworkTask.getId());
-        System.out.println(getHomeworkTask);
+        studyTheoryTask = taskManager.getTask(studyTheoryTask.getId());
+        getHomeworkTask = taskManager.getTask(getHomeworkTask.getId());
+        taskManager.getTask(-1);
         System.out.println();
 
         studyTheoryTask = taskManager.removeTask(studyTheoryTask.getId());
-        System.out.println("Removed task: " + studyTheoryTask);
         printAllTasks(taskManager);
-        studyTheoryTask = taskManager.removeTask(-1);
-        System.out.println("Removed task: " + studyTheoryTask);
+        taskManager.removeTask(studyTheoryTask.getId());
         printAllTasks(taskManager);
         System.out.println();
 
         getHomeworkTask.setDescription("Get access to 4th sprint homework");
         getHomeworkTask.setStatus(TaskStatus.IN_PROGRESS);
-        getHomeworkTask = taskManager.updateTask(getHomeworkTask);
-        if (getHomeworkTask != null) {
-            System.out.println("Task updated");
-        }
+        taskManager.updateTask(getHomeworkTask);
         printAllTasks(taskManager);
         System.out.println();
 
-        taskManager.removeAllTasks();
+        taskManager.removeTasks();
         printAllTasks(taskManager);
         System.out.println();
     }
@@ -70,35 +64,29 @@ public class Main {
         buyHouseEpic.setTitle("Buy a new house");
         buyHouseEpic.setDescription("Buy a new house on Baltic seashore");
 
-        buyCarEpic = taskManager.createNewEpic(buyCarEpic);
-        buyHouseEpic = taskManager.createNewEpic(buyHouseEpic);
+        buyCarEpic = taskManager.addEpic(buyCarEpic);
+        buyHouseEpic = taskManager.addEpic(buyHouseEpic);
         printAllEpics(taskManager);
         System.out.println();
 
-        buyCarEpic = taskManager.getEpicById(buyCarEpic.getId());
-        System.out.println(buyCarEpic);
-        buyHouseEpic = taskManager.getEpicById(buyHouseEpic.getId());
-        System.out.println(buyHouseEpic);
+        buyCarEpic = taskManager.getEpic(buyCarEpic.getId());
+        buyHouseEpic = taskManager.getEpic(buyHouseEpic.getId());
+        taskManager.getEpic(-1);
         System.out.println();
 
         buyCarEpic = taskManager.removeEpic(buyCarEpic.getId());
-        System.out.println("Removed epic: " + buyCarEpic);
         printAllEpics(taskManager);
-        buyCarEpic = taskManager.removeEpic(-1);
-        System.out.println("Removed epic: " + buyCarEpic);
+        taskManager.removeEpic(buyCarEpic.getId());
         printAllEpics(taskManager);
         System.out.println();
 
         buyHouseEpic.setDescription("Buy a new house on the Black Sea");
         buyHouseEpic.setStatus(TaskStatus.IN_PROGRESS);
-        buyHouseEpic = taskManager.updateEpic(buyHouseEpic);
-        if (buyHouseEpic != null) {
-            System.out.println("Epic updated");
-        }
+        taskManager.updateEpic(buyHouseEpic);
         printAllEpics(taskManager);
         System.out.println();
 
-        taskManager.removeAllEpics();
+        taskManager.removeEpics();
         printAllEpics(taskManager);
         System.out.println();
     }
@@ -109,7 +97,7 @@ public class Main {
         Epic kanban = new Epic();
         kanban.setTitle("Kanban application");
         kanban.setDescription("Create backend for task manager");
-        kanban = taskManager.createNewEpic(kanban);
+        kanban = taskManager.addEpic(kanban);
 
         Subtask stepOne = new Subtask();
         stepOne.setTitle("Implement Task class");
@@ -121,20 +109,16 @@ public class Main {
         stepThree.setTitle("Implement Epic class");
         stepThree.setEpicId(kanban.getId());
 
-        stepOne = taskManager.createNewSubtask(stepOne);
-        stepTwo = taskManager.createNewSubtask(stepTwo);
-        stepThree = taskManager.createNewSubtask(stepThree);
-        kanban = taskManager.getEpicById(kanban.getId());
-        System.out.println("Epic: " + kanban);
+        stepOne = taskManager.addSubtask(stepOne);
+        stepTwo = taskManager.addSubtask(stepTwo);
+        stepThree = taskManager.addSubtask(stepThree);
+        kanban = taskManager.getEpic(kanban.getId());
         printAllSubtasks(taskManager);
         System.out.println();
 
-        stepOne = taskManager.getSubtaskById(stepOne.getId());
-        System.out.println(stepOne);
-        stepTwo = taskManager.getSubtaskById(stepTwo.getId());
-        System.out.println(stepTwo);
-        stepThree = taskManager.getSubtaskById(stepThree.getId());
-        System.out.println(stepThree);
+        stepOne = taskManager.getSubtask(stepOne.getId());
+        stepTwo = taskManager.getSubtask(stepTwo.getId());
+        stepThree = taskManager.getSubtask(stepThree.getId());
         System.out.println();
 
         for (Subtask subtask : taskManager.getEpicSubtasks(kanban.getId())) {
@@ -144,44 +128,31 @@ public class Main {
 
         stepOne.setDescription("Make Task class the super class for Epic and Subtask classes");
         stepOne.setStatus(TaskStatus.DONE);
-        stepOne = taskManager.updateSubtask(stepOne);
-        if (stepOne != null) {
-            System.out.println("Subtask updated");
-        }
-        kanban = taskManager.getEpicById(kanban.getId());
-        System.out.println("Epic: " + kanban);
+        taskManager.updateSubtask(stepOne);
+        kanban = taskManager.getEpic(kanban.getId());
         printAllSubtasks(taskManager);
         System.out.println();
 
         stepThree = taskManager.removeSubtask(stepThree.getId());
-        System.out.println("Removed subtask: " + stepThree);
-        kanban = taskManager.getEpicById(kanban.getId());
-        System.out.println("Epic: " + kanban);
+        kanban = taskManager.getEpic(kanban.getId());
         printAllSubtasks(taskManager);
-        stepThree = taskManager.removeSubtask(-1);
-        System.out.println("Removed subtask: " + stepThree);
-        kanban = taskManager.getEpicById(kanban.getId());
-        System.out.println("Epic: " + kanban);
+        taskManager.removeSubtask(stepThree.getId());
+        kanban = taskManager.getEpic(kanban.getId());
         printAllSubtasks(taskManager);
-        stepTwo = taskManager.removeSubtask(stepTwo.getId());
-        System.out.println("Removed subtask: " + stepTwo);
-        kanban = taskManager.getEpicById(kanban.getId());
-        System.out.println("Epic: " + kanban);
+        taskManager.removeSubtask(stepTwo.getId());
+        kanban = taskManager.getEpic(kanban.getId());
         printAllSubtasks(taskManager);
         System.out.println();
 
-        taskManager.removeAllSubtasks();
-        kanban = taskManager.getEpicById(kanban.getId());
-        System.out.println("Epic: " + kanban);
+        taskManager.removeSubtasks();
+        taskManager.getEpic(kanban.getId());
         printAllSubtasks(taskManager);
         System.out.println();
     }
 
     private static void printAllTasks(TaskManager taskManager) {
-        ArrayList<Task> tasks = taskManager.getAllTasks();
-        if (tasks.isEmpty()) {
-            System.out.println("<empty>");
-        } else {
+        ArrayList<Task> tasks = taskManager.getTasks();
+        if (!tasks.isEmpty()) {
             for (Task task : tasks) {
                 System.out.println(task);
             }
@@ -189,10 +160,8 @@ public class Main {
     }
 
     private static void printAllEpics(TaskManager taskManager) {
-        ArrayList<Epic> epics = taskManager.getAllEpics();
-        if (epics.isEmpty()) {
-            System.out.println("<empty>");
-        } else {
+        ArrayList<Epic> epics = taskManager.getEpics();
+        if (!epics.isEmpty()) {
             for (Epic epic : epics) {
                 System.out.println(epic);
             }
@@ -200,10 +169,8 @@ public class Main {
     }
 
     private static void printAllSubtasks(TaskManager taskManager) {
-        ArrayList<Subtask> subtasks = taskManager.getAllSubtasks();
-        if (subtasks.isEmpty()) {
-            System.out.println("<empty>");
-        } else {
+        ArrayList<Subtask> subtasks = taskManager.getSubtasks();
+        if (!subtasks.isEmpty()) {
             for (Subtask subtask : subtasks) {
                 System.out.println(subtask);
             }
--- src/io/github/akuniutka/kanban/service/TaskManager.java
+++ src/io/github/akuniutka/kanban/service/TaskManager.java
@@ -23,22 +23,31 @@ public class TaskManager {
         lastUsedId = 0L;
     }
 
-    public ArrayList<Task> getAllTasks() {
+    public ArrayList<Task> getTasks() {
         LOGGER.logInfo("Retrieving all tasks");
-        return new ArrayList<>(tasks.values());
+        ArrayList<Task> taskList = new ArrayList<>(tasks.values());
+        LOGGER.logInfo(taskList.size() + " task(s) retrieved");
+        return taskList;
     }
 
-    public void removeAllTasks() {
+    public void removeTasks() {
         LOGGER.logInfo("Removing all tasks");
+        LOGGER.logInfo(tasks.size() + " task(s) removed");
         tasks.clear();
     }
 
-    public Task getTaskById(long id) {
+    public Task getTask(long id) {
         LOGGER.logInfo("Retrieving task by id=" + id);
-        return tasks.get(id);
+        Task task = tasks.get(id);
+        if (task == null) {
+            LOGGER.logError("Task not found!");
+        } else {
+            LOGGER.logInfo(task.toString());
+        }
+        return task;
     }
 
-    public Task createNewTask(Task task) {
+    public Task addTask(Task task) {
         LOGGER.logInfo("Creating new task: " + task);
         if (task == null) {
             LOGGER.logError("Cannot create null task!");
@@ -47,6 +56,7 @@ public class TaskManager {
         long taskId = generateId();
         task.setId(taskId);
         tasks.put(taskId, task);
+        LOGGER.logInfo("1 task(s) created");
         return task;
     }
 
@@ -57,39 +67,51 @@ public class TaskManager {
             return null;
         }
         if (!tasks.containsKey(task.getId())) {
-            LOGGER.logError("Cannot update unknown task!");
+            LOGGER.logError("Task not found!");
             return null;
         }
         tasks.put(task.getId(), task);
+        LOGGER.logInfo("1 task(s) updated");
         return task;
     }
 
     public Task removeTask(long id) {
         LOGGER.logInfo("Removing task by id=" + id);
-        if (!tasks.containsKey(id)) {
-            LOGGER.logError("Cannot remove unknown task!");
-            return null;
+        Task task = tasks.remove(id);
+        if (task == null) {
+            LOGGER.logError("Task not found!");
+        } else {
+            LOGGER.logInfo(task.toString());
         }
-        return tasks.remove(id);
+        return task;
     }
 
-    public ArrayList<Epic> getAllEpics() {
+    public ArrayList<Epic> getEpics() {
         LOGGER.logInfo("Retrieving all epics");
-        return new ArrayList<>(epics.values());
+        ArrayList<Epic> epicList = new ArrayList<>(epics.values());
+        LOGGER.logInfo(epicList.size() + " epic(s) retrieved");
+        return epicList;
     }
 
-    public void removeAllEpics() {
+    public void removeEpics() {
         LOGGER.logInfo("Removing all epics");
+        LOGGER.logInfo(epics.size() + " epic(s) (" + subtasks.size() + " subtask(s)) removed");
         subtasks.clear();
         epics.clear();
     }
 
-    public Epic getEpicById(long id) {
+    public Epic getEpic(long id) {
         LOGGER.logInfo("Retrieving epic by id=" + id);
-        return epics.get(id);
+        Epic epic = epics.get(id);
+        if (epic == null) {
+            LOGGER.logError("Epic not found!");
+        } else {
+            LOGGER.logInfo(epic.toString());
+        }
+        return epic;
     }
 
-    public Epic createNewEpic(Epic epic) {
+    public Epic addEpic(Epic epic) {
         LOGGER.logInfo("Creating new epic: " + epic);
         if (epic == null) {
             LOGGER.logError("Cannot create null epic!");
@@ -104,8 +126,9 @@ public class TaskManager {
         for (long subtaskId : epic.getSubtaskIds()) {
             reassignSubtaskToNewEpic(subtasks.get(subtaskId), epicId);
         }
-        recalculateEpicStatus(epic);
+        updateEpicStatus(epic);
         epics.put(epicId, epic);
+        LOGGER.logInfo("1 epic(s) created");
         return epic;
     }
 
@@ -117,7 +140,7 @@ public class TaskManager {
         }
         Epic storedEpic = epics.get(epic.getId());
         if (storedEpic == null) {
-            LOGGER.logError("Cannot update unknown epic!");
+            LOGGER.logError("Epic not found!");
             return null;
         }
         if (containsUnknownSubtasks(epic)) {
@@ -143,7 +166,8 @@ public class TaskManager {
             storedEpic.removeSubtaskId(subtaskId);
             subtasks.remove(subtaskId);
         }
-        recalculateEpicStatus(storedEpic);
+        updateEpicStatus(storedEpic);
+        LOGGER.logInfo("1 epic(s) updated");
         return storedEpic;
     }
 
@@ -151,39 +175,48 @@ public class TaskManager {
         LOGGER.logInfo("Removing epic by id=" + id);
         Epic epic = epics.remove(id);
         if (epic == null) {
-            LOGGER.logError("Cannot remove unknown epic!");
-            return null;
-        }
-        for (long subtaskId : epic.getSubtaskIds()) {
-            LOGGER.logInfo("Removing subtask: " + subtasks.get(subtaskId));
-            subtasks.remove(subtaskId);
+            LOGGER.logError("Epic not found!");
+        } else {
+            LOGGER.logInfo(epic.toString());
+            for (long subtaskId : epic.getSubtaskIds()) {
+                subtasks.remove(subtaskId);
+            }
         }
         return epic;
     }
 
-    public ArrayList<Subtask> getAllSubtasks() {
+    public ArrayList<Subtask> getSubtasks() {
         LOGGER.logInfo("Retrieving all subtasks");
-        return new ArrayList<>(subtasks.values());
+        ArrayList<Subtask> subtaskList = new ArrayList<>(subtasks.values());
+        LOGGER.logInfo(subtaskList.size() + " subtask{s} retrieved");
+        return subtaskList;
     }
 
-    public void removeAllSubtasks() {
+    public void removeSubtasks() {
         LOGGER.logInfo("Removing all subtasks");
         for (Epic epic : epics.values()) {
             ArrayList<Long> subtaskIds = epic.getSubtaskIds();
             for (long subtaskId : subtaskIds) {
                 epic.removeSubtaskId(subtaskId);
             }
-            recalculateEpicStatus(epic);
+            updateEpicStatus(epic);
         }
+        LOGGER.logInfo(subtasks.size() + " subtask(s) removed");
         subtasks.clear();
     }
 
-    public Subtask getSubtaskById(long id) {
+    public Subtask getSubtask(long id) {
         LOGGER.logInfo("Retrieving subtask by id=" + id);
-        return subtasks.get(id);
+        Subtask subtask = subtasks.get(id);
+        if (subtask == null) {
+            LOGGER.logError("Subtask not found!");
+        } else {
+            LOGGER.logInfo(subtask.toString());
+        }
+        return subtask;
     }
 
-    public Subtask createNewSubtask(Subtask subtask) {
+    public Subtask addSubtask(Subtask subtask) {
         LOGGER.logInfo("Creating new subtask: " + subtask);
         if (subtask == null) {
             LOGGER.logError("Cannot create null subtask!");
@@ -198,7 +231,8 @@ public class TaskManager {
         subtask.setId(subtaskId);
         epic.addSubtaskId(subtaskId);
         subtasks.put(subtaskId, subtask);
-        recalculateEpicStatus(epic);
+        updateEpicStatus(epic);
+        LOGGER.logInfo("1 subtask(s) created");
         return subtask;
     }
 
@@ -210,7 +244,7 @@ public class TaskManager {
         }
         Subtask storedSubtask = subtasks.get(subtask.getId());
         if (storedSubtask == null) {
-            LOGGER.logError("Cannot update unknown subtask!");
+            LOGGER.logError("Subtask not found!");
             return null;
         }
         if (!epics.containsKey(subtask.getEpicId())) {
@@ -221,7 +255,8 @@ public class TaskManager {
             reassignSubtaskToNewEpic(storedSubtask, subtask.getEpicId());
         }
         subtasks.put(subtask.getId(), subtask);
-        recalculateEpicStatus(epics.get(subtask.getEpicId()));
+        updateEpicStatus(epics.get(subtask.getEpicId()));
+        LOGGER.logInfo("1 subtask(s) updated");
         return subtask;
     }
 
@@ -229,20 +264,21 @@ public class TaskManager {
         LOGGER.logInfo("Removing subtask by id=" + id);
         Subtask subtask = subtasks.remove(id);
         if (subtask == null) {
-            LOGGER.logError("Cannot remove unknown subtask!");
-            return null;
+            LOGGER.logError("Subtask not found!");
+        } else {
+            LOGGER.logInfo(subtask.toString());
+            Epic epic = epics.get(subtask.getEpicId());
+            epic.removeSubtaskId(subtask.getId());
+            updateEpicStatus(epic);
         }
-        Epic epic = epics.get(subtask.getEpicId());
-        epic.removeSubtaskId(subtask.getId());
-        recalculateEpicStatus(epic);
         return subtask;
     }
 
     public ArrayList<Subtask> getEpicSubtasks(long epicId) {
-        LOGGER.logInfo("Retrieving all subtasks of epic id=" + epicId);
+        LOGGER.logInfo("Retrieving subtasks of epic id=" + epicId);
         Epic epic = epics.get(epicId);
         if (epic == null) {
-            LOGGER.logError("Cannot retrieve subtasks of unknown epic!");
+            LOGGER.logError("Epic not found!");
             return null;
         }
         ArrayList<Long> subtaskIds = epic.getSubtaskIds();
@@ -250,6 +286,7 @@ public class TaskManager {
         for (long subtaskId : subtaskIds) {
             subtaskList.add(subtasks.get(subtaskId));
         }
+        LOGGER.logInfo(subtaskList.size() + " subtask(s) retrieved");
         return subtaskList;
     }
 
@@ -270,11 +307,11 @@ public class TaskManager {
         LOGGER.logInfo("Reassigning subtask to epic id=" + newEpicId + ": " + subtask);
         Epic oldEpic = epics.get(subtask.getEpicId());
         oldEpic.removeSubtaskId(subtask.getId());
-        recalculateEpicStatus(oldEpic);
+        updateEpicStatus(oldEpic);
         subtask.setEpicId(newEpicId);
     }
 
-    private void recalculateEpicStatus(Epic epic) {
+    private void updateEpicStatus(Epic epic) {
         boolean areAllSubtasksNew = true;
         boolean areAllSubtasksDone = true;
         for (long subtaskId : epic.getSubtaskIds()) {
@@ -286,14 +323,12 @@ public class TaskManager {
                 areAllSubtasksDone = false;
             }
         }
-        if (!areAllSubtasksNew) {
-            if (areAllSubtasksDone) {
-                epic.setStatus(TaskStatus.DONE);
-            } else {
-                epic.setStatus(TaskStatus.IN_PROGRESS);
-            }
-        } else {
+        if (areAllSubtasksNew) {
             epic.setStatus(TaskStatus.NEW);
+        } else if (areAllSubtasksDone) {
+            epic.setStatus(TaskStatus.DONE);
+        } else {
+            epic.setStatus(TaskStatus.IN_PROGRESS);
         }
     }
 }
