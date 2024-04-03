a904b26edfa0f52356585abb5ea329195c93d035
Andrei Kuniutka <akuniutka@gmail.com>
2024-04-03 13:40:46 +0300

    refactored utility class Managers: limited it to methods described in requirements

--- src/main/java/io/github/akuniutka/kanban/service/Managers.java
+++ src/main/java/io/github/akuniutka/kanban/service/Managers.java
@@ -4,18 +4,10 @@ public class Managers {
     private Managers() {}
 
     public static TaskManager getDefault() {
-        return getInMemoryTaskManager(getDefaultHistory());
+        return new InMemoryTaskManager(getDefaultHistory());
     }
 
     public static HistoryManager getDefaultHistory() {
-        return getInMemoryHistoryManager();
-    }
-
-    public static TaskManager getInMemoryTaskManager(HistoryManager historyManager) {
-        return new InMemoryTaskManager(historyManager);
-    }
-
-    public static HistoryManager getInMemoryHistoryManager() {
         return new InMemoryHistoryManager();
     }
 }
